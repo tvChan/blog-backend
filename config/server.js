a50e472f4db0b0c782206b08053e53a76b26a55f
tv_Cvt <tv@lovec.at>
2021-06-29 13:07:32 +0800

    feat: init blog-end

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c47ef271ba2aaaaf5392a046f6b980b2'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '81faf846-d472-49e4-b794-20c95be87041'
+};
